42266f642e927c44a70da6a62fc443aec0373e04
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2024-04-09 14:32:33 -0400

    Logged Morgan
    Downloaded and logged morgan to my code.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("common"));
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
